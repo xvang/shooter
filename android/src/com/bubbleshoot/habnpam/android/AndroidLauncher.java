ca7c2b4c6ebda52fbe2ef9c329d9c82a1b490ee1
habnpam <habnpam@gmail.com>
2015-05-13 21:08:02 -0700

    initial commit

---
+++ android/src/com/bubbleshoot/habnpam/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bubbleshoot.habnpam.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bubbleshoot.habnpam.Shooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Shooter(), config);
+	}
+}
---
+++ desktop/src/com/bubbleshoot/habnpam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bubbleshoot.habnpam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bubbleshoot.habnpam.Shooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shooter(), config);
+	}
+}
